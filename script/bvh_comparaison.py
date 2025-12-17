9512aeaba373cf9efcc09ba4a95e815ccddf9f68
AntonuttiAdrien <antonuttiadrien@gmail.com>
2025-12-17 17:09:19 +0100

    update graph with generation comparaison

--- script/bvh_comparaison.py
+++ script/bvh_comparaison.py
@@ -5,6 +5,7 @@ import os
 
 exec = "./bin/cudart"
 ref_exec = "./bin/cudart_ref"
+ref_exec_2 = "./bin/cudart_ref2" #this is not the right reference for the wanted plot.
 
 img_dir = "graphs/"
 data_dir = "tmp/"
@@ -13,6 +14,7 @@ n_obj = [500, 1000, 2000, 4000, 8000, 16000]
 n_obj_accurate = []
 times = []
 ref_times = []
+ref_times_2 = []
 
 def get_time_from_output(output):
     for line in output.strip().split('\n'):
@@ -32,6 +34,7 @@ if not os.path.exists(f'{data_dir}bvh_comparison_times.csv'):
         #lanch subprocess to run the executable
         n_run = 5
         ref_time = 0
+        ref2_time = 0
         opti_time = 0
 
         for _ in range(n_run):
@@ -46,22 +49,28 @@ if not os.path.exists(f'{data_dir}bvh_comparison_times.csv'):
             ref_result = subprocess.run([ref_exec, str(n)], stdout=subprocess.PIPE, text=True)
             ref_time += get_time_from_output(ref_result.stdout)
 
+            #lanch subprocess to run the second reference executable
+            ref_result_2 = subprocess.run([ref_exec_2, str(n)], stdout=subprocess.PIPE, text=True)
+            ref2_time += get_time_from_output(ref_result_2.stdout)
         print(f'Completed for n_obj={n}')
 
         times.append(opti_time / n_run)
         ref_times.append(ref_time / n_run)
+        ref_times_2.append(ref2_time / n_run)
     #save data to file
     print(n_obj_accurate)
-    np.savetxt(f'{data_dir}bvh_comparison_times.csv', np.array([n_obj, n_obj_accurate, times, ref_times]).T, header='n_obj,n_obj_accurate,opti_time,ref_time', delimiter=',')
+    np.savetxt(f'{data_dir}bvh_comparison_times.csv', np.array([n_obj, n_obj_accurate, times, ref_times, ref_times_2]).T, header='n_obj,n_obj_accurate,opti_time,ref_time,ref_time_2', delimiter=',')
 else:
     data = np.loadtxt(f'{data_dir}bvh_comparison_times.csv', skiprows=1, delimiter=',')
     n_obj = data[:,0].astype(int).tolist()
     n_obj_accurate = data[:,1].astype(int).tolist()
     times = data[:,2].tolist()
     ref_times = data[:,3].tolist()
+    ref_times_2 = data[:,4].tolist()
 
-plt.plot(n_obj_accurate, times, '--x', label='Optimized', color='green', alpha=0.8)
+plt.plot(n_obj_accurate, times, '--x', label='BHV and // GPU scene', color='green', alpha=0.8)
 plt.plot(n_obj_accurate, ref_times, '--x', label='Reference', color='blue', alpha=0.8)
+plt.plot(n_obj_accurate, ref_times_2, '--x', label='BHV only', color='red', alpha=0.8)
 plt.xlabel('Number of objects')
 plt.ylabel('Time (s)')
 plt.title('Execution time comparison between\n reference and BVH optimized version')
